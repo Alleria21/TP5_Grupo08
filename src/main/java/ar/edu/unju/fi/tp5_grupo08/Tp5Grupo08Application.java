3b59f7d54890fd50db505e127eff6a933d415932
Rocio <roaguerrero17@gmail.com>
2022-05-05 20:44:50 -0300

    Initial commit

---
+++ src/main/java/ar/edu/unju/fi/tp5_grupo08/Tp5Grupo08Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp5_grupo08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5Grupo08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp5Grupo08Application.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edu/unju/fi/tp5_grupo08/Tp5Grupo08ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp5_grupo08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5Grupo08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
