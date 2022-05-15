1826e9f447e4af1d91e7b78c60673a92c5d10160
nachopad <nacho20002008@gmail.com>
2022-05-15 18:25:44 -0300

    solucion errores del formulario cursos

--- src/main/java/ar/edu/unju/fi/tp5_grupo08/model/Curso.java
+++ src/main/java/ar/edu/unju/fi/tp5_grupo08/model/Curso.java
@@ -1,21 +1,18 @@
 package ar.edu.unju.fi.tp5_grupo08.model;
 
-import java.time.LocalDate;
-
 public class Curso {
 	private int codigo;
 	private String titulo;
 	private String Categoria;
-	private LocalDate fechaInicio;
-	private LocalDate fechaFin;
+	private String fechaInicio;
+	private String fechaFin;
 	private int horas;
 	private String modalidad;
 	private Docente docente;
 	public Curso() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	public Curso(int codigo, String titulo, String categoria, LocalDate fechaInicio, LocalDate fechaFin, int horas,
+	public Curso(int codigo, String titulo, String categoria, String fechaInicio, String fechaFin, int horas,
 			String modalidad, Docente docente) {
 		super();
 		this.codigo = codigo;
@@ -27,7 +24,6 @@ public class Curso {
 		this.modalidad = modalidad;
 		this.docente = docente;
 	}
-	
 	public int getCodigo() {
 		return codigo;
 	}
@@ -46,16 +42,16 @@ public class Curso {
 	public void setCategoria(String categoria) {
 		Categoria = categoria;
 	}
-	public LocalDate getFechaInicio() {
+	public String getFechaInicio() {
 		return fechaInicio;
 	}
-	public void setFechaInicio(LocalDate fechaInicio) {
+	public void setFechaInicio(String fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
-	public LocalDate getFechaFin() {
+	public String getFechaFin() {
 		return fechaFin;
 	}
-	public void setFechaFin(LocalDate fechaFin) {
+	public void setFechaFin(String fechaFin) {
 		this.fechaFin = fechaFin;
 	}
 	public int getHoras() {
@@ -76,5 +72,6 @@ public class Curso {
 	public void setDocente(Docente docente) {
 		this.docente = docente;
 	}
+	
 	
 }
